3dd9886260cd811e46db27069e57b02be8d599e3
Rudhy <rudhy.costa@hotmail.com>
2021-02-09 11:01:44 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+var person = {
+    name:'Rudhy Maycon Pereira da Costa',
+    age:29,
+    alive:true,
+    job:'Procurando',
+    graduation:'Engenharia Civil',
+    height: 1.83 + ' m',
+    weight: 84.5 + ' kg'
+} //Essa é uma variavel do tipo object
+
+console.log(person.height) //Mostra no console o campo height do objeto person
+
+var lista = ['Pera', 'Uva', 'Maçã', 'Salada mista']
+
+console.log(lista[1]) //Mostra no console o ítem 1 da variavel lista
+
+console.log(typeof lista)
+*/
+
+//CONCATENAÇÃO
+
+/*
+var minimo = 1500
+var maximo = 5000
+
+console.log('O preço dos meus Projetos varia de R$ ' + minimo + 'até R$ ' + maximo)
+*/
+
+//OPERADORES
+
+var x=12, y=15
+console.log(Math.pow(y,x))
